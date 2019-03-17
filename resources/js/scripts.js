172aab93a3631bb019d7bb36d4fb659676a1bf03
Sam <sam@meeum.com>
2019-03-17 12:21:41 +1030

    js for navigation

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,1 +1,18 @@
 cssVars();
+
+
+// open and close nav
+
+$('.nav-prompt').click(function()  {
+
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+
+        $('.nav-inner').attr('aria-expanded', 'true');
+
+    } else {
+        $('.nav-inner').attr('aria-expanded', 'false');
+    }
+
+});
+
+
